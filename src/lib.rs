05119ac1511c8319eb71abe4a365a25d76699fd0
jonay2000 <jonabent@gmail.com>
2022-12-01 10:45:21 +0100

    add iter options and all options

--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,72 @@
 use std::mem;
+use std::mem::MaybeUninit;
 
 /// Trait to extend iterables with `max_n` and `min_n`
 pub trait MinMaxN<T: Ord> {
     fn max_n<const N: usize>(self) -> [Option<T>; N];
     fn min_n<const N: usize>(self) -> [Option<T>; N];
+
+    fn min_n_all<const N: usize>(self) -> Option<[T; N]>
+    where
+        Self: Sized,
+    {
+        let src = self.min_n::<N>();
+        let mut dst = [(); N].map(|_| MaybeUninit::uninit());
+        for (d, s) in dst.iter_mut().zip(src) {
+            d.write(s?);
+        }
+
+        // SAFETY:
+        // we either initialized all elements (src and dst have the same length) or we returned (question mark operator)
+        // in case 1, we can assume the entire array is init. In case two we don't run this code.
+        Some(dst.map(|i| unsafe { i.assume_init() }))
+    }
+
+    fn max_n_all<const N: usize>(self) -> Option<[T; N]>
+    where
+        Self: Sized,
+    {
+        let src = self.max_n::<N>();
+        let mut dst = [(); N].map(|_| MaybeUninit::uninit());
+        for (d, s) in dst.iter_mut().zip(src) {
+            d.write(s?);
+        }
+
+        // SAFETY:
+        // we either initialized all elements (src and dst have the same length) or we returned (question mark operator)
+        // in case 1, we can assume the entire array is init. In case two we don't run this code.
+        Some(dst.map(|i| unsafe { i.assume_init() }))
+    }
+
+    fn max_n_iter<const N: usize>(self) -> MinMaxIter<T, N>
+    where
+        Self: Sized,
+    {
+        MinMaxIter(self.max_n(), 0)
+    }
+
+    fn min_n_iter<const N: usize>(self) -> MinMaxIter<T, N>
+    where
+        Self: Sized,
+    {
+        MinMaxIter(self.min_n(), 0)
+    }
+}
+
+pub struct MinMaxIter<T, const N: usize>([Option<T>; N], usize);
+
+impl<T, const N: usize> Iterator for MinMaxIter<T, N> {
+    type Item = T;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        if self.1 >= self.0.len() {
+            return None;
+        }
+
+        let res = self.0[self.1].take();
+        self.1 += 1;
+        res
+    }
 }
 
 fn ilog2(mut num: usize) -> usize {
@@ -107,6 +170,46 @@ where
 mod tests {
     use super::MinMaxN;
     use crate::ilog2;
+
+    #[test]
+    fn max_n_all_test() {
+        let [a, b] = [1, 2, 3, 4].max_n_all().unwrap();
+        assert_eq!(a, 4);
+        assert_eq!(b, 3);
+    }
+
+    #[test]
+    fn min_n_all_test() {
+        let [a, b] = [1, 2, 3, 4].min_n_all().unwrap();
+        assert_eq!(a, 1);
+        assert_eq!(b, 2);
+    }
+
+    #[test]
+    fn min_n_all_test_2() {
+        assert!([1].min_n_all::<2>().is_none());
+    }
+
+    #[test]
+    fn max_n_all_test_2() {
+        assert!([1].max_n_all::<2>().is_none());
+    }
+
+    #[test]
+    fn min_n_iter_test() {
+        let mut res = [1, 2, 3, 4].min_n_iter::<2>();
+        assert_eq!(res.next(), Some(1));
+        assert_eq!(res.next(), Some(2));
+        assert!(res.next().is_none())
+    }
+
+    #[test]
+    fn max_n_iter_test() {
+        let mut res = [1, 2, 3, 4].max_n_iter::<2>();
+        assert_eq!(res.next(), Some(4));
+        assert_eq!(res.next(), Some(3));
+        assert!(res.next().is_none())
+    }
 
     #[test]
     fn max_n_test() {
